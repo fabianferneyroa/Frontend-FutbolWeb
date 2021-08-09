ea2eae6ba0c9f87bdcf6092275b4c0a5e75a9b2c
fabianferneyroa <froaprada@gmail.com>
2021-08-09 09:13:15 -0500

    archivos

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const loginbox = document.getElementById("login-container")
+const menubox = document.getElementById("menu-container")
+
+ function loginScreen() {
+    loginbox.classList.add('d-none')
+    
+}
+
+function menuScreen(){
+    menubox.classList.add('d-none')
+}
