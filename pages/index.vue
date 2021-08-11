2e11b3a7eb8a5e9304c511f04f57755f015335b2
fabianferneyroa <froaprada@gmail.com>
2021-08-11 12:10:06 -0500

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,6 @@
 <template>
   <div>
     <div class="menu-container">
-      <button type="submit">Registrar Partidos</button>
       <nuxt-link class="verde" to="/partidos">Ver Partidos</nuxt-link>
     </div>
   </div>
